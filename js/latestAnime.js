8f5a17671f247fd4e3a2c0e2708195e39a98a7de
John Ernelle Alba <johnernellealbaw@gmail.com>
2023-07-24 06:28:00 +0800

    :bug: debug display of home page

--- js/latestAnime.js
+++ js/latestAnime.js
@@ -17,12 +17,12 @@ function lazyLoad () {
       latestAnimeItem.classList.add('card-anime'); 
       latestAnimeItem.innerHTML = `
                 <div id="card-poster">
-                  <a href="./detail.html?id=${recomAnime.entry.mal_id}&title=${recomAnime.entry.title}">
+                  <a href="./detail.html?id=${newAnime.mal_id}&title=${newAnime.title}">
                     <img src="${newAnime.images.webp.large_image_url}" alt="${newAnime.title}"/>
                   </a>
                 </div>
                 <div class="d-flex flex-column " id="card-detail">
-                    <div id="card-title"><a href="./detail.html?id=${recomAnime.entry.mal_id}&title=${recomAnime.entry.title}">${newAnime.title}</a></div>
+                    <div id="card-title"><a href="./detail.html?id=${newAnime.mal_id}&title=${newAnime.title}">${newAnime.title}</a></div>
                     <div class="d-flex align-content-start gap-1 mt-1" >
                       <div id="card-details">${newAnime.type}</div>
                       <div class="dot"></div>
