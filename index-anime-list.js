b4215f856e97dfe8cd0c3447b0e7f6d5a9f0425d
Ederick S. Banez <esb.tailor@gmail.com>
2023-07-23 11:23:12 +0800

    fix the js for header seach bar and removed the filter part search bar

--- index-anime-list.js
+++ index-anime-list.js
@@ -1,69 +1,88 @@
-// HEADER ON SCROLL SCRIPT 
+// HEADER ON SCROLL SCRIPT
 
-    document.addEventListener('scroll', () =>{
-      const header = document.querySelector('header');
-      if (window.scrollY > 0) {
-        header.classList.add('scrolled');
-      } else {
-        header.classList.remove('scrolled');
+document.addEventListener("scroll", () => {
+  const header = document.querySelector("header");
+  if (window.scrollY > 0) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
 
-      };
-    });
-  
-	fetch('https://api.jikan.moe/v4/anime')
-  .then((res) => {
-    if (res.ok) {
-      return res.json();
-    } else {
-      throw new Error('Network response was not OK.');
-    }
-  }).then((data) => {console.log(data);});
+const APIURL = "https://api.jikan.moe/v4/anime";
+const SEARCHAPI = "https://api.jikan.moe/v4/anime?q=";
 
-	fetch('https://api.jikan.moe/v4/top/anime')
-  .then((res) => {
-    if (res.ok) {
-      return res.json();
-    } else {
-      throw new Error('Network response was not OK.');
-    }
-  })
-  .then((data) => {
-	// Process the data here (e.g., display it or do something else)
-	console.log(data);
-	const animeData = data.data;
-  
-	if (animeData && animeData.length > 0) {
-	  // Display the first 24 anime
-	  const animeCardContainer = document.querySelector('.anime-card-container');
-  
-	  for (let i = 0; i < 24; i++) {
-		const anime = animeData[i];
-  
-		// Create a new card for each anime
-		const cardDiv = document.createElement('div');
-		cardDiv.classList.add('anime-card', 'container-fluid'); // Add .container class here
-		cardDiv.innerHTML = `
-		  <div class="card">
-			<img src="${anime.images.jpg.image_url}" alt="" class="card-img-top">
-			<div class="card-body">
-			  <h5 class="card-title">${anime.title_english}</h5>
-			  <!-- Add other anime information you want to display here -->
-			  <h6 class="Ratings">
-			  	Ratings: ${anime.score}
-				<ul>
-					<li>${anime.year}</li>
-				</ul>
-			  </h6>
-			</div>
+const main = document.getElementById("content");
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+const result = document.getElementById("result");
+
+getMovies(APIURL);
+
+async function getMovies(url) {
+  const resp = await fetch(url);
+  const respData = await resp.json();
+
+  showMovies(respData.data);
+}
+
+function showMovies(movies) {
+  const movieEl = document.createElement("div");
+  main.innerHTML = "";
+  result.innerHTML = "";
+
+  movies.forEach((movie) => {
+    const movieEl = document.createElement("div");
+    movieEl.classList.add("movie");
+
+    movieEl.innerHTML = `<div class="card">
+		  <img src="${movie.images.jpg.image_url}" alt="" class="card-img-top">
+		  <div class="card-body">
+			<h5 class="card-title">${movie.title_english}</h5>
+			<!-- Add other anime information you want to display here -->
+			<h6 class="Ratings">
+				Ratings: ${movie.score}
+			  <ul>
+				  <li>${movie.year}</li>
+			  </ul>
+			</h6>
 		  </div>
-		`;
-  
-		// Append the card to the animeCardContainer
-		animeCardContainer.appendChild(cardDiv);
-	  }
-	}
-  })
-  .catch((error) => {
-	console.error('An error occurred:', error);
+		</div>`;
+    // }
+
+    main.appendChild(movieEl);
   });
-		
+
+  movieEl.innerHTML = `<div>${movies.length} results</div>`;
+  result.appendChild(movieEl);
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const searchTerm = search.value;
+
+  if (searchTerm) {
+    getMovies(SEARCHAPI + searchTerm + "&limit=20");
+
+    search.value = "";
+  }
+});
+
+function selectItem(param1, param2, param3, param4) {
+  const type = param1 === "" ? "" : `type=${param1}`;
+  const status = param2 === "" ? "" : `&status=${param2}`;
+  const ratings = param3 === "" ? "" : `&ratings=${param3}`;
+  const order = param4 === "" ? "" : `&order=${param4}`;
+
+  getMovies(APIURL + "?" + type + status + ratings + order);
+}
+
+document.getElementById("filter").addEventListener("click", function () {
+  const select1Value = document.getElementById("type").value;
+  const select2Value = document.getElementById("status").value;
+  const select3Value = document.getElementById("ratings").value;
+  const select4Value = document.getElementById("order").value;
+
+  selectItem(select1Value, select2Value, select3Value, select4Value);
+});
